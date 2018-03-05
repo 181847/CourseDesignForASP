3929024eab9c831e3af25d66c827a507ea89ffba
181847 <753090084@qq.com>
2018-03-05 19:45:58 +0800

    添加用户数据库类。

---
+++ OnlineAlbum/Helpers/BaseDataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAlbum.Helpers
+{
+    /*!
+        \brief 所有数据库连接类的基类
+    */
+    public abstract class BaseDataBase
+    {
+        /*!
+            \brief 这个应用中用到的连接字符串时同一个
+        */
+        private const string m_connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBForASPCourseDesign;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        protected SqlConnection m_connection;
+        
+        /*!
+            \brief 初始化
+        */
+        public BaseDataBase()
+        {
+            m_connection = new SqlConnection(m_connectString);
+
+            BuildSqlCmds();
+        }
+
+        /*!
+            \brief 创建SqlCommand
+        */
+        abstract protected void BuildSqlCmds();
+    }
+}
---
+++ OnlineAlbum/Helpers/UserDB.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAlbum.Helpers
+{
+    /*
+        \brief 管理用户的注册以及同名用户检查
+     */
+    public class UserDB: BaseDataBase
+    {
+        private SqlCommand m_searchCmd;
+        private SqlCommand m_insertCmd;
+        private SqlCommand m_checkUserAndPasswordCmd;
+
+        /*!
+            \brief 创建SqlCommand
+        */
+        protected override void BuildSqlCmds()
+        {
+            m_searchCmd = new SqlCommand("SELECT count(*) FROM UserTable WHERE USER_ID = @userID", m_connection);
+            m_searchCmd.Parameters.Add("@userID", SqlDbType.Char);
+
+            m_insertCmd = new SqlCommand("INSERT INTO UserTable VALUES (@userID, @userName, @password)", m_connection);
+            m_insertCmd.Parameters.Add("@userID", SqlDbType.Char);
+            m_insertCmd.Parameters.Add("@userName", SqlDbType.NChar);
+            m_insertCmd.Parameters.Add("@password", SqlDbType.Char);
+
+            m_checkUserAndPasswordCmd = new SqlCommand("SELECT count(*) FROM UserTable WHERE USER_ID = @userID AND PASSWORD = @password", m_connection);
+            m_checkUserAndPasswordCmd.Parameters.Add("@userID", SqlDbType.Char);
+            m_checkUserAndPasswordCmd.Parameters.Add("@password", SqlDbType.Char);
+        }
+
+        /*!
+            \brief 注册新用户，注册成功返回true，失败返回false。
+            \param userID 用户的ID，由用户注册时提供
+            \param nickName 用户昵称，用户可以之后修改
+            \param password 密码
+        */
+        public bool Register(string userID, string nickName, string password)
+        {
+            bool success = false;
+
+            try
+            {
+                m_connection.Open();
+                m_insertCmd.Parameters["@userID"].Value = userID;
+                m_insertCmd.Parameters["@userName"].Value = nickName;
+                m_insertCmd.Parameters["@password"].Value = password;
+
+                int count = m_insertCmd.ExecuteNonQuery();
+
+                if (count == 1)
+                {
+                    success = true;
+                }
+            }
+            catch(SqlException ex)
+            {
+                success = false;
+            }
+            finally
+            {
+                m_connection.Close();
+            }
+
+            return success;
+        }
+
+        /*!
+            \brief 检查数据库中是否已经含有同名用户
+            \param userID 注册用户时用到的用户ID
+        */
+        public bool AlreadyHave(string userID)
+        {
+            bool success = false;
+
+            try
+            {
+                m_connection.Open();
+                m_searchCmd.Parameters["@userID"].Value = userID;
+
+                object returnedScalar = m_searchCmd.ExecuteScalar();
+
+                if (returnedScalar == null || returnedScalar.ToString() == "1")
+                {
+                    success = true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                success = false;
+            }
+            finally
+            {
+                m_connection.Close();
+            }
+
+            return success;
+        }
+
+        /*!
+            \brief 检查用户名是否和密码匹配
+            \param userID 用户的ID，由用户注册时提供
+            \param password 密码
+        */
+        public bool CheckUserPassword(string userID, string password)
+        {
+            bool success = false;
+
+            try
+            {
+                m_connection.Open();
+                m_checkUserAndPasswordCmd.Parameters["@userID"].Value = userID;
+                m_checkUserAndPasswordCmd.Parameters["@password"].Value = password;
+
+                int count = (int)m_checkUserAndPasswordCmd.ExecuteScalar();
+
+                if (count == 1)
+                {
+                    success = true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                success = false;
+            }
+            finally
+            {
+                m_connection.Close();
+            }
+
+            return success;
+        }
+    }
+}
