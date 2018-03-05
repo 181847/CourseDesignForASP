9dc7bd16ece71653f545df9bcfec76fe4d6b02b5
181847 <753090084@qq.com>
2018-03-05 22:55:04 +0800

    添加私人图库页面。

---
+++ OnlineAlbum/PersonalPage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PersonalPage.aspx.cs" Inherits="OnlineAlbum.PersonalPage" %>
+<!--
+    PersonalPage 用于给用户管理自己的图片，上传、删除、重命名图片...
+    -->
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="wellcomeUserLbl" runat="server" />
+        <asp:Panel ID="userImgPan" runat="server" />
+        <asp:Button ID="goToMainPageBtn" runat="server" Text="转到主页" />
+        <asp:Button ID="UploadImgBtn" runat="server" Text="上传新图像" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ OnlineAlbum/PersonalPage.aspx.cs
@@ -0,0 +1,40 @@
+﻿using OnlineAlbum.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAlbum
+{
+    public partial class PersonalPage : System.Web.UI.Page
+    {
+        protected string m_userID;
+        protected string m_userName;
+        protected static UserDB m_userDB = new UserDB();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            m_userID = Session["userID"].ToString();
+            m_userName = m_userDB.GetNickNameOf(m_userID);
+
+            wellcomeUserLbl.Text = "欢迎您" + m_userID + "/" + m_userName;
+            
+            if (m_userID == "")
+            {
+                throw new Exception("错误，尚未登陆任何用户，无法查看个人网页");
+            }
+
+            ReadImages();
+        }
+
+        /*!
+            \brief 读取这个用户的所有图像，显示到个人页面上。
+        */
+        protected void ReadImages()
+        {
+
+        }
+    }
+}
