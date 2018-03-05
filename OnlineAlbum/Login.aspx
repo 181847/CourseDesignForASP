e33d28a1ca40fb00aab65f7193359f6237af4eaf
181847 <753090084@qq.com>
2018-03-05 22:54:26 +0800

    完善登陆页面。

---
+++ OnlineAlbum/Login.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="OnlineAlbum.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>登陆界面</p>
+        <table>
+            <tr>
+                <td>
+                    用户名：
+                </td>
+                <td>
+                    <asp:TextBox ID="userIDText" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator runat="server" ErrorMessage="用户名必填" ForeColor="Red" ControlToValidate="userIDText" />
+                    <asp:CustomValidator ID="userIDValidator" runat="server" ControlToValidate="userIDText" OnServerValidate="userIDValidator_ServerValidate" ForeColor="Red" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    密码:
+                </td>
+                <td>
+                    <asp:TextBox ID="passwordText" runat="server" />
+                    <asp:RequiredFieldValidator runat="server" ErrorMessage="密码必填" ForeColor="Red" ControlToValidate="passwordText" />
+                    <asp:Label ID="passwordWarningLbl" runat="server" Text="密码错误" ForeColor="Red" Visible="false"/>
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>
+                    <asp:Button ID="registerBtn" runat="server" Text="注册新用户"  OnClick="registerBtn_Click" CausesValidation="false"/>
+                </td>
+                <td>
+                    <asp:Button ID="loginBtn" runat="server" Text="登陆" OnClick="loginBtn_Click"/>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ OnlineAlbum/Login.aspx.cs
@@ -0,0 +1,68 @@
+﻿using OnlineAlbum.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineAlbum
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        static UserDB m_userDB = new UserDB();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void registerBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~\\Register.aspx");
+        }
+
+        protected void userIDValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string userID = args.Value;
+
+            if ( ! m_userDB.AlreadyHave(userID))
+            {
+                // 用户名不存在的错误。
+                userIDValidator.ErrorMessage = "用户名不存在";
+                args.IsValid = false;
+                // 隐藏密码错误信息
+                passwordWarningLbl.Visible = false;
+                return;
+            }
+
+            // 已知用户名存在，检查密码是否正确
+            if ( ! m_userDB.CheckUserPassword(userID, passwordText.Text))
+            {
+                // 密码错误
+                userIDValidator.ErrorMessage = "";
+                args.IsValid = false;
+                // 显示密码错误信息。
+                passwordWarningLbl.Visible = true;
+                return;
+            }
+
+            // 一切正常。
+            userIDValidator.IsValid = true;
+            passwordWarningLbl.Visible = false;
+        }
+
+        protected void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Session["userID"] = userIDText.Text;
+                Response.Redirect("~\\PersonalPage.aspx");
+            }
+            else
+            {
+                Response.Write("登陆失败<br>");
+            }
+        }
+    }
+}
